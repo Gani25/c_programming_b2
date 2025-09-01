9380f98569622084ad389c298f0ae09533d62e5a
Gani25 <memonabdulgani3d@gmail.com>
2025-09-01 13:14:59 +0530

    Update if12.c

--- if12.c
+++ if12.c
@@ -45,3 +45,23 @@ int main()
         printf("Invalid operator. Please select operator from above range.\n");
     }
 }
+/*
+13. WAP to check whether the number is
+    a. positive -> yes then check whether the number is even or odd
+    b. negative
+    c. zero
+14. WAP to check whether the traingle is
+    a. valid (all sides are positive) -> If valid triangle then check
+        i. Equilateral Triangle (all sides are equal)
+        ii. Isoceles Triangle (any 2 sides are equal)
+        iii. Scalene Triangle (no sides are equal)
+    b. not valid
+15. Electric bill generator based on units consumption:
+    a. units <=100 -> 5/unit
+    b. units between 101 - 200 -> 7/unit
+    c. units > 200 -> 10/unit
+    Example: Unit = 250
+    Amount = First 100 unit (100*5) + next 100 unit(100 * 7) + already 200 amount considered remaining 50 units (50 * 10)
+            500+700+500 = 1700
+
+*/
